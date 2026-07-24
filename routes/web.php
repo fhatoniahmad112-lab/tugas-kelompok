887c4888e74b984f218a6b7a24ca7edb03d34b07
fhatoniahmad112-lab <fhatoniahmad112@gmail.com>
2026-07-24 12:07:05 +0700

    fix duplicate route names by using resource controller

--- routes/web.php
+++ routes/web.php
@@ -4,8 +4,6 @@ use App\Http\Controllers\MobilController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\PenyewaController;
 use App\Http\Controllers\TransaksiController;
-use App\Models\Mobil;        // <--- Tambahkan ini
-use App\Models\Transaksi;    // <--- Tambahkan ini
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -15,14 +13,8 @@ Route::get('/', function () {
 // Semua rute manajemen (Mobil, Penyewa, Transaksi) harus di dalam middleware auth
 Route::middleware(['auth'])->group(function () {
     
-    // Rute Mobil
-    Route::get('/mobil', [MobilController::class, 'index'])->name('mobil.index');
-    Route::resource('mobil', MobilController::class)->middleware(['auth']);
-    Route::get('/mobil/tambah', [MobilController::class, 'create'])->name('mobil.create');
-    Route::post('/mobil/store', [MobilController::class, 'store'])->name('mobil.store');
-    Route::get('/mobil/{id}/edit', [MobilController::class, 'edit'])->name('mobil.edit');
-    Route::put('/mobil/{id}', [MobilController::class, 'update'])->name('mobil.update');
-    Route::delete('/mobil/{id}', [MobilController::class, 'destroy'])->name('mobil.destroy');
+    // Rute Mobil (Menggunakan Resource otomatis)
+    Route::resource('mobil', MobilController::class);
 
     // Rute Penyewa
     Route::get('/penyewa', [PenyewaController::class, 'index'])->name('penyewa.index');
@@ -30,7 +22,6 @@ Route::middleware(['auth'])->group(function () {
     Route::post('/penyewa/store', [PenyewaController::class, 'store'])->name('penyewa.store');
     Route::get('/penyewa/{id}/edit', [PenyewaController::class, 'edit'])->name('penyewa.edit');
     Route::delete('/penyewa/{id}', [PenyewaController::class, 'destroy'])->name('penyewa.destroy');
-    Route::get('/penyewa', [PenyewaController::class, 'index'])->name('penyewa.index');
 
     // Rute Transaksi
     Route::get('/transaksi', [TransaksiController::class, 'index'])->name('transaksi.index');
@@ -44,7 +35,6 @@ Route::middleware(['auth'])->group(function () {
 Route::get('/dashboard', function () {
     return view('dashboard', [
         'totalMobil' => \App\Models\Mobil::count(),
-        // Ubah logika: Hitung yang statusnya TIDAK 'dikembalikan'
         'sedangDipinjam' => \App\Models\Transaksi::where('status', '!=', 'dikembalikan')->count()
     ]);
 })->middleware(['auth'])->name('dashboard');
